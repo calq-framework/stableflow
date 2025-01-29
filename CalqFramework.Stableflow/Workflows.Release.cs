ab1e72d2f77ec4272b9d7d61a5730f6826d675e2
greg-chuchro <gchuchro@acm.org>
2025-01-29 10:51:55 +0900

    push all

--- CalqFramework.Stableflow/Workflows.Release.cs
+++ CalqFramework.Stableflow/Workflows.Release.cs
@@ -9,7 +9,10 @@ partial class Workflows {
         }
         var modifiedProjectFiles = GetChangedProjectFiles(latestTagHash);
         if (TryGetBumpedVersion(modifiedProjectFiles, latestTagHash, out var version)) {
-            BuildPushTag(modifiedProjectFiles, version, true);
+            // BuildPushTag(modifiedProjectFiles, version, true);
+            // FIXME dotnet pack -p:Version={version} also sets version for subprojects so when when a package is referrenced the transition packages are required to be of the same version
+            // logic to determine all transition packages of modified packages is TODO so for now push everything
+            BuildPushTag(GetProjectFiles(), version, true);
         } else {
             // assembly files haven't changed
             TagAsLatest();
